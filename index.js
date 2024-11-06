fa70d3b6c8221cd685f5e0aeac8604abf8620d3f
mohamed fathy <mohammedfathyzaky@gmail.com>
2024-11-07 01:47:44 +0200

    final commit in part 3

--- index.js
+++ index.js
@@ -1,114 +1,107 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
-
-const app=express();
-const fs = require('fs');
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const path= './data.json';
-const date=new Date();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+const app=express()
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const date=new Date()
 const corsOptions = {
-    origin: 'http://localhost:5173',
-};
+  origin: 'http://localhost:5173',
+}
 morgan.token('response-body', (req, res) => {
-    return JSON.stringify(res.json) || ''; 
-});
+  return JSON.stringify(res.json) || ''
+})
 
 
 const id_error = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
   
-    if (error.name === 'CastError') {
-      return response.status(400).send( 'malformatted id' )
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({ data: error.message })
-    }
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send( 'malformatted id' )
   }
-
-const fetch_error= (request, response,next) => {
-    response.status(400).send( 'error with database' )
-    next(error)
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ data: error.message })
+  }
+  next(error)
 }
 
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send( 'unknown endpoint' )
-  }
+const fetch_error= (request, response) => {
+  response.status(400).send( 'error with database' )
 
-const Person = require('./modules/mongodb');
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send( 'unknown endpoint' )
+}
 
-app.use(express.json());
-app.use(cors(corsOptions));
+const Person = require('./modules/mongodb')
+
+app.use(express.json())
+app.use(cors(corsOptions))
 app.use(express.static('dist'))
 app.use((req, res, next) => {
+  const originalJson = res.json
+  res.responseBody = null
     
-    const originalJson = res.json;
+  res.json = function (body) {
+    if(body.security=='classfied'){
+      res.responseBody={data:'classfied'}
+    }
+    else{
+      console.log(body.security)
+      res.responseBody = body.data
+    }
+    return originalJson.call(this, body.data)
+  }
 
-  
-    res.responseBody = null;
-    
-    res.json = function (body) {
-        if(body.security=='classfied'){
-            res.responseBody={data:'classfied'};
-        }
-        else{
-            console.log(body.security);
-            res.responseBody = body.data; 
-        }
-        return originalJson.call(this, body.data); 
-    };
-
-    next();
-});
+  next()
+})
 
 morgan.token('response-body', (req, res) => {
-    return JSON.stringify(res.responseBody) || ''; 
-});
+  return JSON.stringify(res.responseBody) || ''
+})
 
-const customFormat = ':method :url :status :response-time ms :response-body';
+const customFormat = ':method :url :status :response-time ms :response-body'
 
-app.use(morgan(customFormat));
+app.use(morgan(customFormat))
 
-app.get('/info',(request,response)=>{
+app.get('/info',(request,response,next)=>{
 
 
-   const current_time=[date.getFullYear(),date.getMinutes(),date.getSeconds()].join(':');
-   const current_date=`${days[date.getDay()]} ${months[date.getMonth()]} ${String(date.getDate()).padStart(2, '0')} ${date.getFullYear()} ${current_time} GMT${date.getTimezoneOffset()}`
-   Person.find({}).then(persons => {
-    const message=`<p>Phonebook has info for ${persons.length} people</P> <p>${current_date}</P>`;
-    response.send({data:message,security:'valid'});
-    }).catch(error=>{
-    next(error);
-     }) 
+  const current_time=[date.getFullYear(),date.getMinutes(),date.getSeconds()].join(':')
+  const current_date=`${days[date.getDay()]} ${months[date.getMonth()]} ${String(date.getDate()).padStart(2, '0')} ${date.getFullYear()} ${current_time} GMT${date.getTimezoneOffset()}`
+  Person.find({}).then(persons => {
+    const message=`<p>Phonebook has info for ${persons.length} people</P> <p>${current_date}</P>`
+    response.send({data:message,security:'valid'})
+  }).catch(error=>{
+    next(error)
+  }) 
   
    
-});
+})
 app.get('/api/persons/:id', (request, response,next) => {
-    Person.findById(request.params.id).then(person => {
-        if(person){
-            response.json({data:person,security:'vaild'});
-        }
-        else{
-            console.log("what");
-            response.status(404).send('no person with this id');
-        }
-      }).catch(error=>{
-        next(error);
-      })
-});
+  Person.findById(request.params.id).then(person => {
+    if(person){
+      response.json({data:person,security:'vaild'})
+    }
+    else{
+      console.log('what')
+      response.status(404).send('no person with this id')
+    }
+  }).catch(error=>{
+    next(error)
+  })
+})
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response,next) => {
   
-    Person.findByIdAndDelete(request.params.id)
+  Person.findByIdAndDelete(request.params.id)
     .then(result => {
       if(result){
         response.status(204).end()
       }
       else{
-        response.status(404).send('no person with this id');
+        response.status(404).send('no person with this id')
       }
     })
     .catch(error =>  next(error))
@@ -117,48 +110,48 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.get('/api/persons', (request, response,next) => {
 
-    Person.find({}).then(result => {
-        response.json({data:result,security:'classfied'});
-      }) .catch(error=>{
-        next(error);
-      }) 
-});
+  Person.find({}).then(result => {
+    response.json({data:result,security:'classfied'})
+  }) .catch(error=>{
+    next(error)
+  }) 
+})
 
 app.post('/api/persons', (request, response,next) => {
-    const {name,number}=request.body;
-    console.log(request.body);
-    const person = new Person({
-        name: name,
-        number: number
-      })
-      person.save().then(result => {
-        console.log('note saved!')
-        response.json({data:result,security:'valid'});
-      }).catch(error=>{
-       next(error);
-      })
+  const {name,number}=request.body
+  console.log(request.body)
+  const person = new Person({
+    name: name,
+    number: number
+  })
+  person.save().then(result => {
+    console.log('note saved!')
+    response.json({data:result,security:'valid'})
+  }).catch(error=>{
+    next(error)
+  })
     
     
-});
+})
 app.put('/api/persons/:id', (request,response,next) =>{
-    const {name,number}=request.body;
-    console.log(request.body);
-    const person ={
-        name:name,
-        number:number
-    }
-    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+  const {name,number}=request.body
+  console.log(request.body)
+  const person ={
+    name:name,
+    number:number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then(new_person => {
       response.json({data:new_person})
     })
     .catch(error => next(error))
 })
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-app.use(id_error);
+app.use(id_error)
 
-app.use(fetch_error);
+app.use(fetch_error)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- modules/mongodb.js
+++ modules/mongodb.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url =process.env.MONGODB_URI;
+const url =process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
   .then(result => {
+    console.log(result)
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -12,31 +13,31 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(value) {
+        const phone_regex=/^\d{2,3}-\d+$/
+        return phone_regex.test(value)
       },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: function(value) {
-              const phone_regex=/^\d{2,3}-\d+$/
-              return phone_regex.test(value);
-            },
-            message: 'invlaid phone number form'
-          }
-      },
-  })
+      message: 'invlaid phone number form'
+    }
+  },
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-  module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema)
